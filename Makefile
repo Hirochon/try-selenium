5eb2564b2e8486e8e17146de6c04e48cbbc699c0
Hirochon <heacet43@gmail.com>
2022-11-14 01:53:17 +0900

    add

---
+++ Makefile
@@ -0,0 +1,2 @@
+selenium:
+	docker run -d -p 4444:4444 -p 7900:7900 --shm-size="2g" selenium/standalone-chrome:107.0-20221104
--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("hogeee")
+from selenium import webdriver
+
+# Chrome のオプションを設定する
+options = webdriver.ChromeOptions()
+options.add_argument('--headless')
+
+# Selenium Server に接続する
+driver = webdriver.Remote(
+    command_executor='http://localhost:4444/wd/hub',
+    options=options,
+)
+
+# Selenium 経由でブラウザを操作する
+driver.get('https://qiita.com')
+print(driver.current_url)
+
+# ブラウザを終了する
+driver.quit()
